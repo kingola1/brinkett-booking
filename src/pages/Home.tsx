e7aef146ac8a0c8085d76fd32d97d40cd9bcc016
kingola1 <segun8427@gmail.com>
2025-07-05 15:35:47 +0100

    Changed location to katampe

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -186,7 +186,7 @@ const Home: React.FC = () => {
 								</div>
 								<div className="flex items-center space-x-2">
 									<MapPin className="w-5 h-5" />
-									<span>Kado, Abuja</span>
+									<span>Katampe, Abuja</span>
 								</div>
 							</div>
 							<Link
